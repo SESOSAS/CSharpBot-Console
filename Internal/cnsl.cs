f14a4553eef12909518dbfc281fab9d91c6f5335
90Hz <80009481+SESOSAS@users.noreply.github.com>
2022-05-01 11:12:34 +0200

    Commit
    Added Client Start
    Added Client Stop
    Added SendMessage
    Added Change RPC [Not Working]
    Added Close Application by Command

---
+++ Internal/cnsl.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBot_Console.Internal
+{
+    internal class cnsl
+    {
+        /// <summary>
+        /// Console output with text
+        /// </summary>
+        public static void Output(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        /// <summary>
+        /// Console output with arg text
+        /// </summary>
+        public static void Output(string[] text)
+        {
+            Console.WriteLine(text);
+        }
+
+        /// <summary>
+        /// Console output with text and Console color
+        /// </summary>
+        public static void Output(string text, ConsoleColor consoleColor)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+
+        /// <summary>
+        /// Console output with arg text and Console color
+        /// </summary>
+        public static void Output(string[] text, ConsoleColor consoleColor)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// Console Error
+        /// </summary>
+        public static void Error(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(string.Format("[ERROR]: ", text));
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// Console arg Error
+        /// </summary>
+        public static void Error(string[] text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(string.Format("[ERROR]: ", text));
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// Console Warning
+        /// </summary>
+        public static void Warning(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(string.Format("[WARNING]: ", text));
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// Console arg Warning
+        /// </summary>
+        public static void Warning(string[] text)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(string.Format("[WARNING]: ", text));
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        /// <summary>
+        /// Console give back strings written in console
+        /// </summary>
+        public static string Await()
+        {
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Clears the console
+        /// </summary>
+        public static void Clear()
+        {
+            Console.Clear();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using CSharpBot_API;
+using CSharpBot_Console.Internal;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace CSharpBot_Console
+{
+    internal class Program
+    {
+        public static List<string> MenuSetup = new List<string>();
+
+        public static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                cnsl.Error("Can't run a session and use a file!");
+            }
+            GenerateMenu();
+            Menu();
+        }
+
+        public static void GenerateMenu()
+        {
+            MenuSetup.Add("Options: \n - Start\n - Stop\n - RPC\n - Send *channelID* *text*\n - Exit");
+        }
+
+        public static void Menu()
+        {
+            cnsl.Clear();
+            foreach (string s in MenuSetup)
+            {
+                cnsl.Output(s);
+            }
+            string input = cnsl.Await();
+            string[] inputArgs = cnsl.Await().Split(" ");
+            string inputArgsStringPOne = input.Replace(inputArgs[0], "").Replace(inputArgs[1], "");
+            string command = inputArgs[0].ToLower();
+            switch (command)
+            {
+                case "start":
+                    DBot.DCClientStart();
+                    Menu();
+                    break;
+                case "stop":
+                    DBot.DCClientStop();
+                    Menu();
+                    break;
+                case "rpc":
+                    Menu();
+                    break;
+                case "send":
+                    DBot.SendMessage(inputArgs[1], inputArgsStringPOne);
+                    Menu();
+                    break;
+                case "exit":
+                    DBot.DCClientStop();
+                    Process.GetCurrentProcess().Kill();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+        }
+    }
+}
